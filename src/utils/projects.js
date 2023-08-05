647fdee1d09eacbc1ceb6836152ebd646b878ffa
Caesario Dito <caesariodito@gmail.com>
2023-08-05 17:18:24 +0700

    Update projects.js
    adjust the content

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,35 +1,57 @@
 const projects = [
   {
     image:
-      "https://cdn.discordapp.com/attachments/1105720623143063614/1127662255517999254/image.png",
-    title: "Project 1",
-    description: "This is a description of Project 1.",
-    tags: ["tag1", "tag2", "tag3"],
-    link: "https://www.google.com",
+      "https://cdn.discordapp.com/attachments/1137319747202068510/1137322146906001428/image.png",
+    title: "BDC Competition",
+    description:
+      "Archive from the BDC Satria Data 2023 Competition | Automatic License Plate Recognition Task",
+    tags: ["Yolo", "CompVis", "DL"],
+    link: "https://techwaste-ml-preview.streamlit.app/",
   },
   {
     image:
-      "https://cdn.discordapp.com/attachments/1105720623143063614/1129722840132890674/image.png",
-    title: "Project 2",
-    description: "This is a description of Project 2.",
-    tags: ["tag4", "tag5", "tag6"],
-    link: "https://www.google.com",
+      "https://cdn.discordapp.com/attachments/1137319747202068510/1137323643941167224/ov17HjAZ8T5VkAAAAASUVORK5CYII.png",
+    title: "Gammafest Competition",
+    description:
+      "The demo of image classification and summarization capabilities of the Techwaste project.",
+    tags: ["ML", "DL", "Tensorflow"],
+    link: "https://github.com/caesariodito/comp-gammafest/tree/main/comp",
   },
   {
     image:
-      "https://cdn.discordapp.com/attachments/1105720623143063614/1127662255517999254/image.png",
-    title: "Project 3",
-    description: "This is a description of Project 3.",
-    tags: ["tag1", "tag2", "tag3"],
-    link: "https://www.google.com",
+      "https://cdn.discordapp.com/attachments/1137319747202068510/1137320977395626034/d690166ec89671481b79b85fa907e4cdcc5935cfd28afd468f90a452.png",
+    title: "Techwaste ML",
+    description:
+      "The demo of image classification and summarization capabilities of the Techwaste project.",
+    tags: ["ML", "DL", "Tensorflow"],
+    link: "https://techwaste-ml-preview.streamlit.app/",
   },
   {
     image:
-      "https://cdn.discordapp.com/attachments/1105720623143063614/1129722840132890674/image.png",
-    title: "Project 4",
-    description: "This is a description of Project 4.",
-    tags: ["tag4", "tag5", "tag6"],
-    link: "https://www.google.com",
+      "https://cdn.discordapp.com/attachments/1137319747202068510/1137324699735564408/assignment-automation.png",
+    title: "Assignment Automation",
+    description:
+      "A student project designed to demonstrate the power of AI technology in generating answers and insights. (Automate Assignments)",
+    tags: ["GPT", "automation", "OCR"],
+    link: "https://assignment-automation.streamlit.app/",
+  },
+  {
+    image:
+      "https://cdn.discordapp.com/attachments/1137319747202068510/1137319810519277578/image.png",
+    title: "Management System",
+    description:
+      "A system information management system that can be used to manage information about natural disaster, volunteering, and others.",
+    tags: ["Laravel", "web-app"],
+    link: "http://relawan.yeu.or.id/",
+  },
+  {
+    image:
+      "https://cdn.discordapp.com/attachments/1137319747202068510/1137326600992587807/slot20machine2077720demo.png",
+    title: "Slot Machine Probability",
+    description:
+      "A simple demonstration of probability in the slot machine that consist triple number 7. This was a 3rd-semester college assignment and no longer hosted.",
+    tags: ["simulation", "probability"],
+    link: "https://github.com/sheeshario/k-777prob-machine/tree/main",
   },
 ];
 
