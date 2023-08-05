364ed5b34257100a9a569920f6e8d15cf7369b8c
Caesario Dito <caesariodito@gmail.com>
2023-08-05 19:13:59 +0700

    adjust projects and images content

--- src/utils/images.js
+++ src/utils/images.js
@@ -15,19 +15,69 @@ export const images = [
     height: "993",
   },
   {
-    src: "https://cdn.discordapp.com/attachments/1105720623143063614/1135558403708686479/IMG-20230625-WA0000.jpg",
-    width: "1227",
+    src: "https://cdn.discordapp.com/attachments/1137319747202068510/1137340989019799673/IMG-20230629-WA0015.png",
+    width: "744",
+    height: "993",
+  },
+  {
+    src: "https://cdn.discordapp.com/attachments/1137319747202068510/1137340734958211102/IMG-20230629-WA0030.png",
+    width: "744",
+    height: "993",
+  },
+  {
+    src: "https://cdn.discordapp.com/attachments/1137319747202068510/1137334967085576242/IMG-20230618-WA0011.png",
+    width: "1200",
     height: "590",
   },
   {
-    src: "https://cdn.discordapp.com/attachments/1105720623143063614/1135557037867155466/IMG_6081.jpg",
-    width: "1227",
+    src: "https://cdn.discordapp.com/attachments/1137319747202068510/1137334950417403914/IMG-20230616-WA0004.png",
+    width: "744",
+    height: "993",
+  },
+  {
+    src: "https://cdn.discordapp.com/attachments/1137319747202068510/1137335158723330048/IMG_0867.jpg",
+    width: "744",
+    height: "993",
+  },
+  {
+    src: "https://cdn.discordapp.com/attachments/1137319747202068510/1137340681317269556/image-16.png",
+    width: "1647",
     height: "920",
   },
   {
-    src: "https://cdn.discordapp.com/attachments/1105720623143063614/1135557811108069407/IMG-20230629-WA0045.jpg",
-    width: "744",
-    height: "993",
+    src: "https://cdn.discordapp.com/attachments/1137319747202068510/1137341462879670302/IMG-20230520-WA0014.jpg",
+    width: "1647",
+    height: "920",
+  },
+  {
+    src: "https://cdn.discordapp.com/attachments/1137319747202068510/1137335755556003861/IMG-20221220-WA0028.jpg",
+    width: "1247",
+    height: "920",
+  },
+  {
+    src: "https://cdn.discordapp.com/attachments/1137319747202068510/1137341713116057620/IMG-20230520-WA0017.jpg",
+    width: "1647",
+    height: "920",
+  },
+  {
+    src: "https://cdn.discordapp.com/attachments/1137319747202068510/1137335279250833489/IMG-20221205-WA0029.jpg",
+    width: "1247",
+    height: "920",
+  },
+  {
+    src: "https://cdn.discordapp.com/attachments/1137319747202068510/1137346571583889478/1682222933286.png",
+    width: "800",
+    height: "590",
+  },
+  {
+    src: "https://cdn.discordapp.com/attachments/1137319747202068510/1137343088260546560/1682510151541.png",
+    width: "1447",
+    height: "920",
+  },
+  {
+    src: "https://cdn.discordapp.com/attachments/1137319747202068510/1137343303512236052/Screenshot_2023-08-05-18-15-53-50_1c337646f29875672b5a61192b9010f9.jpg",
+    width: "1300",
+    height: "1000",
   },
 ];
 
--- src/utils/projects.js
+++ src/utils/projects.js
@@ -4,7 +4,7 @@ const projects = [
       "https://cdn.discordapp.com/attachments/1137319747202068510/1137322146906001428/image.png",
     title: "BDC Competition",
     description:
-      "Archive from the BDC Satria Data 2023 Competition | Automatic License Plate Recognition Task",
+      "Archive from the BDC Satria Data 2023 Competition | Automatic License Plate Recognition Task.",
     tags: ["Yolo", "CompVis", "DL"],
     link: "https://techwaste-ml-preview.streamlit.app/",
   },
@@ -13,7 +13,7 @@ const projects = [
       "https://cdn.discordapp.com/attachments/1137319747202068510/1137323643941167224/ov17HjAZ8T5VkAAAAASUVORK5CYII.png",
     title: "Gammafest Competition",
     description:
-      "The demo of image classification and summarization capabilities of the Techwaste project.",
+      "Necessary, Insightful information towards the GAMMAFEST competition.",
     tags: ["ML", "DL", "Tensorflow"],
     link: "https://github.com/caesariodito/comp-gammafest/tree/main/comp",
   },
@@ -28,10 +28,28 @@ const projects = [
   },
   {
     image:
+      "https://cdn.discordapp.com/attachments/1137319747202068510/1137340008517337099/Google_Cloud_Platform-Logo.png",
+    title: "Model Deployment",
+    description:
+      "Experimenting with deployments on the model (boilerplate for cloud computing). Created API with FastAPI and deploy it on Google Cloud Platform for the Techwaste Project.",
+    tags: ["ML", "DL", "Tensorflow"],
+    link: "https://github.com/Techwaste/try-deploying-model",
+  },
+  {
+    image:
+      "https://cdn.discordapp.com/attachments/1137319747202068510/1137339759371493396/image.png",
+    title: "Techwaste Pitching",
+    description:
+      "The Bangkit 2023 Capstone Project Pitching of Techwaste. Showcasing our features, technology, and stacks of our project.",
+    tags: ["ML", "DL", "Tensorflow"],
+    link: "https://www.youtube.com/watch?v=sUSjhU3-FDk",
+  },
+  {
+    image:
       "https://cdn.discordapp.com/attachments/1137319747202068510/1137324699735564408/assignment-automation.png",
     title: "Assignment Automation",
     description:
-      "A student project designed to demonstrate the power of AI technology in generating answers and insights. (Automate Assignments)",
+      "Personal project designed to demonstrate the power of AI technology in generating answers and insights. (Automate Assignments).",
     tags: ["GPT", "automation", "OCR"],
     link: "https://assignment-automation.streamlit.app/",
   },
@@ -49,7 +67,7 @@ const projects = [
       "https://cdn.discordapp.com/attachments/1137319747202068510/1137326600992587807/slot20machine2077720demo.png",
     title: "Slot Machine Probability",
     description:
-      "A simple demonstration of probability in the slot machine that consist triple number 7. This was a 3rd-semester college assignment and no longer hosted.",
+      "Simple demonstration of probability in the slot machine that consist triple number 7. This was a 3rd-semester college assignment and no longer hosted.",
     tags: ["simulation", "probability"],
     link: "https://github.com/sheeshario/k-777prob-machine/tree/main",
   },
