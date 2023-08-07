f3efa95979376f40873a8b4cb21e943f08b43170
Caesario Dito <caesariodito@gmail.com>
2023-08-07 12:49:36 +0700

    fix routing "/projects"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import Projects from "./components/Projects";
 
 // vercel analytics
 import { Analytics } from "@vercel/analytics/react";
@@ -16,7 +15,6 @@ function App() {
           <Router>
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/projects" element={<Projects />} />
             </Routes>
           </Router>
         </div>
