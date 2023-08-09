0543fa5b3bf34b22ac3e69b5935c1a40a3d92dec
Caesario Dito <caesariodito@gmail.com>
2023-08-09 11:04:10 +0700

    fix layouting on mobile view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ import projects from "../utils/projects";
 function Home() {
   return (
     <div className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row m-auto mt-8 lg:mx-auto">
-      <div className="main flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+      <div className="main flex-auto min-w-0 mt-6 flex flex-col px-2 mx-4 md:px-0">
         <Navbar />
         <Description />
         <Projects projects={projects} />
